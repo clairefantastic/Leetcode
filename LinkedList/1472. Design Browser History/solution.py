bd396535ae8ae6b58ad6649270938fa88d7c877f
Claire Chiou <clairefantastic7@gmail.com>
2024-08-25 16:57:55 +0800

    Add problem 1472 array solution

--- LinkedList/1472. Design Browser History/solution.py
+++ LinkedList/1472. Design Browser History/solution.py
@@ -24,3 +24,26 @@ class BrowserHistory(object):
             self.cur = self.cur.next
             steps -= 1
         return self.cur.val
+
+class BrowserHistory(object):
+
+    def __init__(self, homepage):
+        self.i = 0
+        self.len = 1
+        self.history = [homepage]
+
+    def visit(self, url):
+        if len(self.history) < self.i + 2:
+            self.history.append(url)
+        else:
+            self.history[self.i + 1] = url
+        self.i += 1
+        self.len = self.i + 1
+
+    def back(self, steps):
+        self.i = max(self.i - steps, 0)
+        return self.history[self.i]
+
+    def forward(self, steps):
+        self.i = min(self.i + steps, self.len - 1)
+        return self.history[self.i]
